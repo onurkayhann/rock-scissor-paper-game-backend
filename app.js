fcb7c56cdac5a5dbf96884363ff591601dcb7764
Onur Kayhan <onur.kayhan@chasacademy.se>
2024-02-11 18:04:04 +0100

    backend for current date

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const moment = require('moment');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/currentdate', (req, res) => {
+    res.send(moment().format('MM/DD/YYYY HH:mm:ss'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
