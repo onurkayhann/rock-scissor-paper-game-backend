5fe3f118ebe9bd64f258e504675ba4213764dd58
Onur Kayhan <onur.kayhan@chasacademy.se>
2024-02-12 14:09:18 +0100

    add comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// imports
 const express = require('express');
 const moment = require('moment');
 const cors = require('cors');
@@ -5,12 +6,15 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+// implementing cors
 app.use(cors());
 
+// endpoint to call from frontend
 app.get('/currentdate', (req, res) => {
     res.send(moment().format('MM/DD/YYYY HH:mm:ss'));
 });
 
+// app listen
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
